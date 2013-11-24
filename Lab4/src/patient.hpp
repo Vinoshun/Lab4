c799c94eea8e6560445d673479db658c9794129d
Vino M <vinoshun.manoratnam@ryerson.ca>
2013-11-24 18:06:48 -0500

    patient header

--- Lab4/src/patient.hpp
+++ Lab4/src/patient.hpp
@@ -7,8 +7,20 @@
 
 #ifndef PATIENT_HPP_
 #define PATIENT_HPP_
+#include <string>
+#include <iostream>
+using namespace std;
 
-
+class patient{
+	string fName;
+	string lName;
+	int age;
+	string SIN;
+public:
+	patient;
+	void display;
+	~patient;
+};
 
 
 #endif /* PATIENT_HPP_ */
